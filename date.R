2fb133da85597bae06a19f4e5f0a11dc4c7d2f79
soumyajitz <2014soumyajit@gmail.com>
2016-02-23 18:00:48 -0600

    Dates

---
+++ date.R
@@ -0,0 +1,8 @@
+d1 =date()
+class(d1)
+d2 = Sys.Date()
+class(d2)
+# %d is day, %a is abbreviated weekday, %A is unabbreviated weekday,%m is month,%b is abbreviated month, %B is unabbreviated month
+# %Y is year
+format(d2,"%a %b %d %Y")
+
