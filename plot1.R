ccdddb284bf5c87ccfb1fe17bcae998f52ecdec9
soumyajitz <2014soumyajit@gmail.com>
2016-05-19 18:25:16 -0500

    Plotting System Basic Examples

--- plot1.R
+++ plot1.R
@@ -1,7 +1,15 @@
+# Base Plotting System
 library(datasets)
 data(cars)
 with(cars,plot(speed,dist))
 
+# Lattice Plotting System
 library(lattice)
 state <- data.frame(state.x77,region = state.region)
 xyplot(Life.Exp ~ Income | region,data = state,layout =c(4,1))
+
+# GGPlot() System
+library(ggplot2)
+data(mpg)
+qplot(displ,hwy,data=mpg)
+
